c200460febf234454da389251275b9bbc5ea4cbc
josiefwood <36741732+josiefwood@users.noreply.github.com>
2018-04-19 15:50:17 -0500

    Made some comparison tables

--- Code/Code Rework.R
+++ Code/Code Rework.R
@@ -52,12 +52,9 @@ no.pid <- 60
 
 pop.mod <- glmer(MissedDose ~ Q7 + ZAlcTox + Day + Q7*Day + (1|PID), data = comp, family=binomial(link=logit))
 
-# pop.pids <- list()
-pop.mod.coeffs <- summary(pop.mod)$coef[,1:2] # if glmer object: take the first row of coefficients, excluding the intercept, the betas are the same for each PID but the intercept varies.
+pop.mod.coeffs <- summary(pop.mod)$coef[,1:2]
 
 # Correlation Matrix
-# Use model.matrix to convert factor variables to dummy encoding
-# Matches OG matrix!!!
 corrmat <- cor(model.matrix(~.-1,data=comp[,c('ZEduc', 'ZIncom45', 'Age', 'ZAlcTox', 'ZCESDFU', 'ZAUDIT')]))
 round(corrmat, 2) # display the matrix, rounding values to 2 decimal places
 
@@ -102,17 +99,7 @@ for (i in 1:no.sim) {
   X[X$Q7==0,c('Q8')] <- 0
   
   # Make Missed Dose
-  # This is where issues come in!!!
-  # Closest I could get to these numbers was:
-  # MD <- glm(data=comp,MissedDose~ZEduc+ZIncom45+Age+ZAlcTox+ZCESDFU+ZAUDIT,family=binomial(link=logit))
-  # MD <- glm(data=comp,MissedDose~Q7+ZAlcTox+Day+Q7*Day,family=binomial(link=logit))
   MD <- glmer(MissedDose ~ Q7 + ZAlcTox + Day + Q7*Day + (1|PID), data = comp, family=binomial(link=logit))
-  # Use a predict function instead of coding in the values
-  # int <- MD@beta[1]
-  # b1 <- MD@beta[2] # DrinkYN1
-  # b2 <- MD@beta[3] # Day
-  # b3 <- MD@beta[4] # ZAlcTox
-  # b4 <- MD@beta[5] # DrinkYN1:Day
   sigmaPID <- MD@theta
   
   X$groupErr <- rnorm(60, mean=0, sd=sigmaPID)[X$PID]
@@ -124,22 +111,9 @@ for (i in 1:no.sim) {
   sim.data[[i]] <- X
 }
 
-sim.mods <- list()
-sim.mod.intercepts <- list()
-sim.mod.coeffs <- list()
-
-for (i in 1:no.sim){
-  sim.mods[[i]] <- glmer(MissedDose ~ Q7 + ZAlcTox + Day + Q7*Day + (1|PID), data = as.data.frame(sim.data[i]), family=binomial(link=logit))
-  #sim.mod.intercepts[[i]] <- coef(sim.mods[[i]])[[1]][1]
-  sim.mod.coeffs[[i]] <- summary(sim.mods[[i]])$coef[,1:2] # take the first row and only the last 4 columns to get coefficients for everything but the intercepts
-}
-
-
 #Remove unnecessary before analysis
 rm(comp, sigma, X, b1, b2,b3,b4, fit1, fit2, fit3, fit4, fit5, fit6, fit7,
    i, int, sigmaPID)
-
-l1 <- sim.data[[1]]
 
 ## Create MISSINGNESS
 # rremove <- function(nrem, x) { # this literally just randomly removes nrem columns. Like, the entire column. Why???
@@ -175,23 +149,21 @@ wave.pm <- lapply(sim.data,function(x){wave.des(x,11)})
 no.imp = 5 # number of imputed datasets
 
 # Step 2 - Impute simulated datasets, for both methods
-split.imp <- lapply(split.pm, function(x) {mice(x, m=no.imp)}) # loop through the split.imps in the complete function
-split.data <- list()
-for (i in 1:no.sim){
-  split.data[[i]] <- list()
-  for (j in 1:no.imp){
-  split.data[[i]][[j]] <- complete(split.imp[[i]],j)
+
+impute <- function(pm){ # pass split.data
+  imp <- lapply(pm, function(x) {mice(x, m=no.imp)}) # loop through the split.imps in the complete function
+  res.data <- list()
+  for (i in 1:no.sim){
+    res.data[[i]] <- list()
+    for (j in 1:no.imp){
+      res.data[[i]][[j]] <- complete(imp[[i]],j)
+    }
   }
+  res.data
 }
 
-wave.imp <- lapply(wave.pm, function(x) {mice(x, m=no.imp)})
-wave.data <- list()
-for (i in 1:no.sim){
-  wave.data[[i]] <- list()
-  for (j in 1:no.imp){
-    wave.data[[i]][[j]] <- complete(wave.imp[[i]],j)
-  }
-}
+split.data <- impute(split.pm)
+wave.data <- impute(wave.pm)
 
 # Step 3 - Build models from imputed simulation data for both methods
 # Result: a list of lists of model coefficients for each method
@@ -208,34 +180,18 @@ mod.maker <- function(dat){ # passing split.data[[i]]
   }
   return(list(betas=modbetas,se=modse))
 }
-# df <- data.frame(split.data[[1]][[1]])
-# model <- glmer(MissedDose ~ DrinkYN*Day + Q1 + Q2 + Q3 + Q4 + Q5 + Q6 + Q7 + (1|PID), data = df, family=binomial(link="logit"))
-# mod <- mod.maker(split.data[[1]]) # <-- every person has their own intercept??? the coefficients are the same except ints
 
-set.seed(12345)
-splitmods <- list()
-for (i in 1:no.sim){
-  splitmods[i] <- list(mod.maker(split.data[[i]]))
+pm.mods <- function(dat){
+  set.seed(12345)
+  resmods <- list()
+  for (i in 1:no.sim){
+    resmods[i] <- list(mod.maker(dat[[i]]))
+  }
+  resmods
 }
 
-set.seed(12345)
-wavemods <- list()
-for (i in 1:no.sim){
-  wavemods[i] <- list(mod.maker(wave.data[[i]])) # also fails to converge
-}
-
-# Step 4 - Calculate mean parameter estimates for both methods
-# compare means of coefficents overall and then find means of each person's intercept and compare those to original???
-split.coeff.means <- list()
-wave.coeff.means <- list()
-
-for (i in 1:no.sim){ # not working
-    split.coeff.means[[i]] <- colMeans(do.call(rbind, splitmods[[i]]$betas)) # using split.coeff.means[[i]] throws an error about the subscript being out of bounds, but if run with split.coeff.means[i] and THEN double brackets, it works???
-}
-
-for (i in 1:no.sim){
-  wave.coeff.means[[i]] <- colMeans(do.call(rbind, wavemods[[i]]$betas))
-}
+splitmods <- pm.mods(split.data)
+wavemods <- pm.mods(wave.data)
 
 # COVERAGE #
 
@@ -254,8 +210,6 @@ TD <- function(mods){
   }
   intervals
 }
-
-TD(splitmods)
 
 split.intervals <- TD(splitmods)
 wave.intervals <- TD(wavemods)
@@ -338,10 +292,16 @@ fmi <- function(mods){
 split.fmi <- fmi(splitmods)
 wave.fmi <- fmi(wavemods)
 
+# COMPARISON TABLE #
+# coverage, bias, mse, fmi for each variable, each method
 
+split.table <- data.frame(split.cover[2],cbind(split.bias,split.mse),split.fmi)
+colnames(split.table) <- c('Coverage','Bias','MSE','FMI')
+split.table
 
-
-
+wave.table <- data.frame(wave.cover[2],cbind(wave.bias,wave.mse),wave.fmi)
+colnames(wave.table) <- c('Coverage','Bias','MSE','FMI')
+wave.table
 
 
 
