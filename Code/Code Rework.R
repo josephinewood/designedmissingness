bb82fe41812e093db60c4aa2dfd2ecec2401d8f9
josiefwood <36741732+josiefwood@users.noreply.github.com>
2018-04-19 15:13:24 -0500

    Added functions for Coverage, Bias, MSE, and FMI of variables

--- Code/Code Rework.R
+++ Code/Code Rework.R
@@ -237,315 +237,137 @@ for (i in 1:no.sim){
   wave.coeff.means[[i]] <- colMeans(do.call(rbind, wavemods[[i]]$betas))
 }
 
+# COVERAGE #
 
-intervals <- list()
-for (i in 1:no.sim){
-Q <- apply(do.call(rbind,splitmods[[i]]$betas),2,mean)
-B <- apply(do.call(rbind,splitmods[[i]]$betas),2,var)
-W <- apply(do.call(rbind,splitmods[[i]]$se)^2,2,mean)
-
-#Combioned standard error
-Tm <- sqrt((1+1/no.imp)*B + W)
-df <- (no.imp - 1) * (1 + 1/no.imp*W/B)^2
-
-intervals[[i]] <- cbind(Q ,lower = Q - qt(0.025,df)*Tm, upper = Q + qt(0.025,df)*Tm)
-
-}
-
-
-split.TD <- TD(splitmods,split.coeff.means)
-wave.TD <- TD(wavemods,wave.coeff.means)
-
-split.allcis <- list()    # FIX IF NEED CIs FOR ALL VARIABLES, NOT JUST Q7
-for (j in 1:5){
-  allci <- list()
+TD <- function(mods){
+  intervals <- list()
   for (i in 1:no.sim){
-    Est <- unlist(split.coeff.means[[i]][j])
-    L <- c(unlist(split.coeff.means[[i]][j]) - unlist(split.TD[j]))
-    U <- c(unlist(split.coeff.means[[i]][j]) + unlist(split.TD[j]))
-    allci[[i]] <- cbind(Est,L,U)
+    Q <- apply(do.call(rbind,mods[[i]]$betas),2,mean)
+    B <- apply(do.call(rbind,mods[[i]]$betas),2,var)
+    W <- apply(do.call(rbind,mods[[i]]$se)^2,2,mean)
+    
+    #Combioned standard error
+    Tm <- sqrt((1+1/no.imp)*B + W)
+    df <- (no.imp - 1) * (1 + 1/no.imp*W/B)^2
+    
+    intervals[[i]] <- cbind(Q ,lower = Q - qt(0.975,df)*Tm, upper = Q + qt(0.975,df)*Tm)
   }
-  split.allcis[[j]] <- allci
+  intervals
 }
 
+TD(splitmods)
 
+split.intervals <- TD(splitmods)
+wave.intervals <- TD(wavemods)
 
-# # Std. Error = summary(sim.mods[[1]])$coefficients[i,2] where i is the row of the variable you care about
-# sim.allstd.errors <- list() # USE IF CARE ABOUT ALL VARIABELS, NOT JUST Q7
-# for (i in 1:no.sim){
-#    for (j in 2:5){ # number of variables is 5
-#      sim.allstd.errors[[i]][j-1] <- summary(sim.mods[[i]])$coefficients[j,2] # subscripting error again
-#    }
-#  }
-# 
-# sim.allcis <- list()    # FIX IF NEED CIs FOR ALL VARIABLES, NOT JUST Q7
-# for (i in 1:no.sim){
-#    allci <- list()
-#    for (j in 1:4){
-#      Est <- sim.mod.coeffs[[i]][j]
-#      L <- c(sim.mod.coeffs[[i]][j] - sim.allstd.errors[[i]][j])
-#      U <- c(sim.mod.coeffs[[i]][j] + sim.allstd.errors[[i]][j])
-#      allci[[j]] <- cbind(Est,L,U)
-#    }
-#    sim.allcis[[i]] <- allci
-# }
-
-# # USE IF CARE ABOUT ALL OF THE VARIABLES
-# allsplit.fits <- list() # lists of lists of whether or not a parameter estimate fell in the sim's CI
-# for (i in 1:no.sim){
-#   incl <- list()
-#   for (j in 1:4){
-#     incl[[j]] <- (split.coeff.means[[i]][[1]][j] >= sim.allcis[[i]][[j]][[2]]) & (split.coeff.means[[i]][[1]][j] <= sim.allcis[[i]][[j]][[3]])
-#   }
-#   allsplit.fits[[i]] <- incl
-# }
-# 
-
-
-
-analyze.comp <- function(df, ...) {
-  fit <- glm(data=df, MissedDose~Q7+ZAlcTox+Day+Q7*Day, family=binomial(link=logit))
-  g0.est <- summary(fit)$coefficients[1,1]
-  g0.se <- summary(fit)$coefficients[1,2]
-  g0.L <- g0.est - 1.96*g0.se
-  g0.U <- g0.est + 1.96*g0.se
-  b1.est <- summary(fit)$coefficients[2,1]
-  b1.se <- summary(fit)$coefficients[2,2]
-  b1.L <- b1.est - 1.96*b1.se
-  b1.U <- b1.est + 1.96*b1.se
-  b2.est <- summary(fit)$coefficients[3,1]
-  b2.se <- summary(fit)$coefficients[3,2]
-  b2.L <- b2.est - 1.96*b2.se
-  b2.U <- b2.est + 1.96*b2.se
-  b3.est <- summary(fit)$coefficients[4,1]
-  b3.se <- summary(fit)$coefficients[4,2]
-  b3.L <- b3.est - 1.96*b3.se
-  b3.U <- b3.est + 1.96*b3.se
-  c(g0.est, g0.L, g0.U, b1.est, b1.L, b1.U, b2.est, b2.L, b2.U, b3.est, b3.L, b3.U)
+coverage <- function(intervals){
+  fits <- list() # lists of lists of whether or not a parameter estimate fell in the sim's CI
+  for (i in 1:no.sim){
+    incl <- list()
+    for (j in 1:5){
+      incl[[j]] <- (pop.mod.coeffs[j] >= intervals[[i]][j,2]) & (pop.mod.coeffs[j] <= intervals[[i]][j,3])
+    }
+    fits[[i]] <- incl
+  }
+  
+  fit.mat <- matrix(unlist(allsplit.fits),5)
+  cover <- apply(splitmat,2,mean)
+  return(list(fit.mat,cover))
 }
 
-# library(beepr)
-# beep(2, expr= 
-
-full.res <- lapply(sim.data, function(x) {analyze.comp(x)})
+split.cover <- coverage(split.intervals)
+wave.cover <- coverage(wave.intervals)
 
 
-mean(unlist(lapply(full.res, function(x) {abs(x[10]-(0.2))})))
-mean(unlist(lapply(full.res, function(x) {abs(x[10]-(0.2))/0.2})))          # What is this???
-mean(unlist(lapply(full.res, function(x) {(x[10]-(0.2))**2})))
-mean(unlist(lapply(full.res, function(x) {(x[11]<0.2)&(x[12]>0.2)})))
-mean(unlist(lapply(full.res, function(x) {x[12]-x[11]})))
+# BIAS #
 
-
-mi.ml <- function(set,...) {
-  fit <- with(set, glmer(MissedDose~DrinkYN+ZAlcTox+Day+(1 |PID), family=binomial(link=logit),
-                         control=glmerControl(optimizer='bobyqa', optCtrl = list(maxfun=2000))))
-  p <- pool(fit)
-  res <- summary(p)
-  c(res[1,1], res[1,6], res[1,7], res[1,9], res[2,1], res[2,6], res[2,7], res[2,9],
-    res[3,1], res[3,6], res[3,7], res[3,9], res[4,1], res[4,6], res[4,7], res[4,9])
+bias <- function(intervals){
+  results <- list()
+  numerator <- list()
+  Qdiffs <- list()
+  for (j in 1:no.imp){
+    Qdiffs[[j]] <- list(NA,5)
+    for (i in 1:no.sim){
+      Qdiffs[[j]][i] <- unlist(intervals[[i]][j,1]) - unlist(pop.mod.coeffs[j,1])
+    }
+    numerator[j] <- Reduce("+",Qdiffs[[j]])
+    results[j] <- unlist(numerator[j])/no.sim
+  }
+  results
 }
 
-fit <- with(split.imp, glmer(MissedDose~DrinkYN+ZAlcTox+Day+(1 |PID), family=binomial(link=logit),
-                       control=glmerControl(optimizer='bobyqa', optCtrl = list(maxfun=2000))))
-fit2 <- with(imp2, glmer(MissedDose~DrinkYN+ZAlcTox+Day+(1 |PID), family=binomial(link=logit),
-                         control=glmerControl(optimizer='bobyqa', optCtrl = list(maxfun=2000))))
-p <- pool(fit)
-p2 <- pool(fit2)
+split.bias <- bias(split.intervals)
+wave.bias <- bias(wave.intervals)
 
+# MSE #
 
-sf2.low.res <- lapply(imp2[[1]], function(x) {mi.ml(x)})
-sf2.med.res <- lapply(imp2[[2]], function(x) {mi.ml(x)})
-sf2.high.res <-lapply(imp2[[3]], function(x) {mi.ml(x)})
-rm(imp2)
-
-wd.low.res <- lapply(imp3[[1]], function(x) {mi.ml(x)})
-wd.med.res <- lapply(imp3[[2]], function(x) {mi.ml(x)})
-wd.high.res <-lapply(imp3[[3]], function(x) {mi.ml(x)})
-rm(imp3)
-
-
-
-mean(unlist(lapply(sf2.low.res, function(x) {abs(x[9]-(0.64))})))
-mean(unlist(lapply(sf2.med.res, function(x) {abs(x[9]-(0.64))})))
-mean(unlist(lapply(sf2.high.res, function(x) {abs(x[9]-(0.64))})))
-
-mean(unlist(lapply(sf2.low.res, function(x) {abs(x[9]-(0.64))/0.64})))
-mean(unlist(lapply(sf2.med.res, function(x) {abs(x[9]-(0.64))/0.64})))
-mean(unlist(lapply(sf2.high.res, function(x) {abs(x[9]-(0.64))/0.64})))
-
-mean(unlist(lapply(sf2.low.res, function(x) {(x[9]-(0.64))**2})))
-mean(unlist(lapply(sf2.med.res, function(x) {(x[9]-(0.64))**2})))
-mean(unlist(lapply(sf2.high.res, function(x) {(x[9]-(0.64))**2})))
-
-mean(unlist(lapply(sf2.low.res, function(x) {(x[10]<0.64)&(x[11]>0.64)})))
-mean(unlist(lapply(sf2.med.res, function(x) {(x[10]<0.64)&(x[11]>0.64)})))
-mean(unlist(lapply(sf2.high.res, function(x) {(x[10]<0.64)&(x[11]>0.64)})))
-
-mean(unlist(lapply(sf2.low.res, function(x) {x[11]-x[10]})))
-mean(unlist(lapply(sf2.med.res, function(x) {x[11]-x[10]})))
-mean(unlist(lapply(sf2.high.res, function(x) {x[11]-x[10]})))
-
-mean(unlist(lapply(sf2.low.res, function(x) {x[12]})))
-mean(unlist(lapply(sf2.med.res, function(x) {x[12]})))
-mean(unlist(lapply(sf2.high.res, function(x) {x[12]})))
-
-
-
-##SIMULATE WITH LESS INTER-SURVEY RELATIONSHIP
-
-#Find fit to simulate
-fit1 <- glm(data=comp, DrinkYN~ZIncom45+Age+ZAlcTox+ZAUDIT, family=binomial(link=logit))
-fit2<- glm(data=comp, Q1~ZEduc+ZIncom45+ZAlcTox+ZCESDFU, family=binomial(link=logit))
-fit3<- glm(data=comp, Q2~ZEduc+ZIncom45+Age+ZAlcTox
-           +ZCESDFU+ZAUDIT+Gender, family=binomial(link=logit))
-fit4<- glm(data=comp, Q3~ZEduc+ZIncom45+ZAlcTox
-           +ZCESDFU+Gender, family=binomial(link=logit))
-fit5<- glm(data=comp, Q4~ZEduc+ZAlcTox
-           +ZCESDFU+ZAUDIT+Gender, family=binomial(link=logit))
-fit6<- glm(data=comp, Q5~ZEduc+ZIncom45+ZAlcTox
-           +ZAUDIT, family=binomial(link=logit))
-fit7<- glm(data=comp, Q6~ZEduc+ZIncom45+Age+ZAlcTox
-           +ZCESDFU+ZAUDIT, family=binomial(link=logit))
-
-#Prep for simulate
-sigma <- matrix(c(1.0, 0.3, 0.0,-0.1, 0.1,-0.2,
-                  0.3, 1.0,-0.1,-0.1, 0.0, 0.1,
-                  0.0,-0.1, 1.0, 0.0,-0.1, 0.0,
-                  -0.1,-0.1, 0.0, 1.0, 0.1, 0.0,
-                  0.1, 0.0,-0.1, 0.1, 1.0, 0.4,
-                  -0.2, 0.1, 0.0, 0.0, 0.4, 1.0), nrow=6)
-
-
-#Simulate
-set.seed(2982364)
-sim.data <- list()
-for (i in 1:200) {
-  X <- as.data.frame(rmvnorm(60, mean=rep(0,6), sigma=sigma))
-  names(X) <- c('ZEduc', 'ZIncom45', 'Age', 'ZAlcTox', 'ZCESDFU', 'ZAUDIT')
-  X$PID <- 1:60
-  X$Gender <- as.factor(rbinom(60, 1, prob=0.5))
-  X <- X[rep(row.names(X), 45),] #Repeat each row 45 times, all curent variables are time invarient
-  X <- X[order(X$PID),] #Order by PID
-  X$Day <- rep(0:44, 60) #Create a variable containing day variable 0-44
-  
-  
-  #Create Variables
-  X$DrinkYN <- 1/(1+exp(-1*predict(fit1, newdata = X)))
-  X$DrinkYN <- as.factor(rbinom(2700, 1, prob = X$DrinkYN))
-  X$Q1 <- 1/(1+exp(-1*predict(fit2, newdata=X)))
-  X$Q1 <- as.factor(rbinom(2700, 1, prob = X$Q1))
-  X$Q2 <- 1/(1+exp(-1*predict(fit3, newdata = X)))
-  X$Q2 <- as.factor(rbinom(2700,1,prob=X$Q2))
-  X$Q3 <- 1/(1+exp(-1*predict(fit4, newdata = X)))
-  X$Q3 <- as.factor(rbinom(2700,1,prob=X$Q3))
-  X$Q4 <- 1/(1+exp(-1*predict(fit5, newdata = X)))
-  X$Q4 <- as.factor(rbinom(2700,1,prob=X$Q4))
-  X$Q5 <- 1/(1+exp(-1*predict(fit6, newdata = X)))
-  X$Q5 <- as.factor(rbinom(2700,1,prob=X$Q5))
-  X$Q6 <- 1/(1+exp(-1*predict(fit7, newdata = X)))
-  X$Q6 <- as.factor(rbinom(2700,1,prob=X$Q6))
-  X$Q7 <- rpois(2700, 2)
-  X[X$DrinkYN==0,c('Q7')] <- 0
-  
-  #Make Missed Dose
-  int <- -2.3
-  b1 <- 0.56
-  b2 <- 1.28
-  b3 <- 0.2
-  sigmaPID <- 1
-  
-  X$groupErr <- rnorm(60, mean=0, sd=sigmaPID)[X$PID]
-  X$groupErr2 <- rnorm(60, mean=0, sd=sigmaPID)[X$PID]
-  X$MissedDose <- int +b1*as.numeric(as.character(X$DrinkYN))+b2*X$ZAlcTox+b3*X$Day+X$groupErr
-  X$MissedDose <- 1/(1+exp(-X$MissedDose))
-  X$MissedDose <- rbinom(2700, 1, prob=X$MissedDose)
-  
-  sim.data[[i]] <- X
+mse <- function(intervals){
+  results <- list()
+  numerator <- list()
+  Qdiffs2 <- list()
+  for (j in 1:no.imp){
+    Qdiffs2[[j]] <- list(NA,5)
+    for (i in 1:no.sim){
+      Qdiffs2[[j]][i] <- (unlist(intervals[[i]][j,1]) - unlist(pop.mod.coeffs[j,1]))^2
+    }
+    numerator[j] <- Reduce("+",Qdiffs2[[j]])
+    results[j] <- unlist(numerator[j])/no.sim
+  }
+  results
 }
 
+split.mse <- mse(split.intervals)
+wave.mse <- mse(wave.intervals)
 
-#Remove unnecessary before analysis
-rm(comp, sigma, X, b1, b2,b3, fit1, fit2, fit3, fit4, fit5, fit6, fit7,
-   i, int, sigmaPID)
+# (1+(1/D))B/TD
 
+# FMI #
 
-
-full.res <- lapply(sim.data, function(x) {analyze.comp(x)})
-
-mean(unlist(lapply(full.res, function(x) {abs(x[10]-(0.2))})))
-mean(unlist(lapply(full.res, function(x) {abs(x[10]-(0.2))/0.2})))
-mean(unlist(lapply(full.res, function(x) {(x[10]-(0.2))**2})))
-mean(unlist(lapply(full.res, function(x) {(x[11]<0.2)&(x[12]>0.2)})))
-mean(unlist(lapply(full.res, function(x) {x[12]-x[10]})))
-
-
-
-
-
-##SIMULATE WITH NO INTER-SURVEY RELATIONSHIP
-
-
-#Prep for simulate
-sigma <- matrix(c(1.0, 0.3, 0.0,-0.1, 0.1,-0.2,
-                  0.3, 1.0,-0.1,-0.1, 0.0, 0.1,
-                  0.0,-0.1, 1.0, 0.0,-0.1, 0.0,
-                  -0.1,-0.1, 0.0, 1.0, 0.1, 0.0,
-                  0.1, 0.0,-0.1, 0.1, 1.0, 0.4,
-                  -0.2, 0.1, 0.0, 0.0, 0.4, 1.0), nrow=6)
-
-
-#Simulate
-set.seed(2982364)
-sim.data <- list()
-for (i in 1:200) {
-  X <- as.data.frame(rmvnorm(60, mean=rep(0,6), sigma=sigma))
-  names(X) <- c('ZEduc', 'ZIncom45', 'Age', 'ZAlcTox', 'ZCESDFU', 'ZAUDIT')
-  X$PID <- 1:60
-  X$Gender <- as.factor(rbinom(60, 1, prob=0.5))
-  X <- X[rep(row.names(X), 45),] #Repeat each row 45 times, all curent variables are time invarient
-  X <- X[order(X$PID),] #Order by PID
-  X$Day <- rep(0:44, 60) #Create a variable containing day variable 0-44
-  
-  
-  #Create Variables
-  X$DrinkYN <- as.factor(rbinom(2700, 1, prob = 0.22))
-  X$Q1 <- as.factor(rbinom(2700, 1, prob = 0.17))
-  X$Q2 <- as.factor(rbinom(2700,1,prob=0.15))
-  X$Q3 <- as.factor(rbinom(2700,1,prob=0.24))
-  X$Q4 <- as.factor(rbinom(2700,1,prob=0.22))
-  X$Q5 <- as.factor(rbinom(2700,1,prob=0.12))
-  X$Q6 <- as.factor(rbinom(2700,1,prob=0.18))
-  X$Q7 <- rpois(2700, 2)
-  X[X$DrinkYN==0,c('Q7')] <- 0
-  
-  #Make Missed Dose
-  int <- -2.3
-  b1 <- 0.56
-  b2 <- 1.28
-  b3 <- 0.2
-  sigmaPID <- 1
-  
-  X$groupErr <- rnorm(60, mean=0, sd=sigmaPID)[X$PID]
-  X$groupErr2 <- rnorm(60, mean=0, sd=sigmaPID)[X$PID]
-  X$MissedDose <- int +b1*as.numeric(as.character(X$DrinkYN))+b2*X$ZAlcTox+b3*X$Day+X$groupErr
-  X$MissedDose <- 1/(1+exp(-X$MissedDose))
-  X$MissedDose <- rbinom(2700, 1, prob=X$MissedDose)
-  
-  sim.data[[i]] <- X
+fmi <- function(mods){
+  for (i in 1:no.sim){
+    B <- apply(do.call(rbind,mods[[i]]$betas),2,var)
+    W <- apply(do.call(rbind,mods[[i]]$se)^2,2,mean)
+    
+    # Combined variance
+    TD <- (1 + 1/no.imp)*B + W
+    FMI <- ((1 + (1/no.imp))*B)/TD
+  }
+  FMI
 }
 
-
-#Remove unnecessary before analysis
-rm(comp, sigma, X, b1, b2,b3
-   i, int, sigmaPID)
+split.fmi <- fmi(splitmods)
+wave.fmi <- fmi(wavemods)
 
 
 
-full.res <- lapply(sim.data, function(x) {analyze.comp(x)})
 
-mean(unlist(lapply(full.res, function(x) {abs(x[10]-(0.2))})))
-mean(unlist(lapply(full.res, function(x) {abs(x[10]-(0.2))/0.2})))
-mean(unlist(lapply(full.res, function(x) {(x[10]-(0.2))**2})))
-mean(unlist(lapply(full.res, function(x) {(x[11]<0.2)&(x[12]>0.2)})))
-mean(unlist(lapply(full.res, function(x) {x[12]-x[10]})))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
